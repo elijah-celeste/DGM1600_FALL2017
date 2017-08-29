1807258120970274a60b6de74614d035748789e6
Unknown <elijah.celeste@gmail.com>
2017-08-28 20:14:20 -0600

    Added Unity Project Files

---
+++ DGM1600_Game/Assets/Hello_World.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello_World : MonoBehaviour {
+
+	string message = "Help Me!";
+	float testNumber = 1;
+
+	// Use this for initialization
+	void Start () {
+		print("Hello World");
+		print(message);
+		print("Hello World " + testNumber + 12 + " " + message);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print(testNumber+=1);
+	}
+}
