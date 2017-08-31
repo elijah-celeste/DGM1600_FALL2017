fc78fc67341b8dd71a3addb9fcfa56dc21567e74
Unknown <elijah.celeste@gmail.com>
2017-08-30 20:11:18 -0600

    Added Unity Project files

---
+++ DGM1600_Game/Assets/Classes.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Classes : MonoBehaviour {
+
+	public class One {
+		public int money = 1000000;
+		public float fuel = 0.01f;
+
+		private string name = "Bob";
+	}
+	
+	public class Two {
+		
+		int basePlayerHealth = 100;
+		int baseEnemyHealth = 200;
+	}
+
+	public class Three {
+
+		string enemyName = "Koopa";
+		int a = 9;
+		int b = a + 1;
+	}
+
+}
